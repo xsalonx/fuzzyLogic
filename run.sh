48c9c0fe0ef45e63d88af0374bdd071f37a4b8a5
xsalonx <lukasz.dubiel3@gmail.com>
2022-01-14 14:30:53 +0100

    script not compile

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ JARPATH="./target/PSI_jFuzzyLogic-1.0.1-jar-with-dependencies.jar"
 CONTROLLER_PATH="./FCL/controller.fcl"
 
 
-if [ "$1" = "-nc" ]; then
+if [ "$1" = "-n" ]; then
   java -jar $JARPATH $CONTROLLER_PATH;
 else
   mvn clean package
